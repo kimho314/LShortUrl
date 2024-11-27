2b8a6f92d97fa2140ef1f0d5b66b039198ff2323
hoseop.kim <hoseop.kim@cjconnect.io>
2024-11-27 10:08:45 +0900

    init project

---
+++ LShortUrl/src/main/java/com/example/lshorturl/LShortUrlApplication.java
@@ -0,0 +1,13 @@
+package com.example.lshorturl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LShortUrlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LShortUrlApplication.class, args);
+    }
+
+}
---
+++ LShortUrl/src/test/java/com/example/lshorturl/LShortUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lshorturl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LShortUrlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
