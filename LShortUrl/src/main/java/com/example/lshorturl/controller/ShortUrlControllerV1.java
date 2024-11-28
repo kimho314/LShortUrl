79624bbae102e31c0cb9411a211db88f3ba1894e
kimho314 <hoseop.kim@cjconnect.io>
2024-11-28 14:11:01 +0900

    [feat] : modify getLongUrl request parameter type

--- LShortUrl/src/main/java/com/example/lshorturl/controller/ShortUrlControllerV1.java
+++ LShortUrl/src/main/java/com/example/lshorturl/controller/ShortUrlControllerV1.java
@@ -8,29 +8,36 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/v1")
 @RequiredArgsConstructor
 public class ShortUrlControllerV1 {
+
     private final ShortUrlServiceV1 shortUrlServiceV1;
 
+    /**
+     * @title 단축 url 생성
+     */
     @PostMapping("/shortens")
-    public ResponseEntity<SaveShortenUrlResponseDto> saveShortenUrl(@RequestBody SaveShortenUrlRequestDto request){
+    public ResponseEntity<SaveShortenUrlResponseDto> saveShortenUrl(@RequestBody SaveShortenUrlRequestDto request) {
         SaveShortenUrlResponseDto shortenUrl = shortUrlServiceV1.shortenUrl(request);
-        return ResponseEntity.created(URI.create("")).body(shortenUrl);
+        return ResponseEntity.ok(shortenUrl);
     }
 
-    @GetMapping("/shortens/{shortUrl}")
-    public ResponseEntity<Void> getLongUrl(@PathVariable String shortUrl){
+    /**
+     * @title 원본 url redirect
+     */
+    @GetMapping("/shortens")
+    public ResponseEntity<Void> getLongUrl(@RequestParam(value = "shortUrl") String shortUrl) {
         String longUrl = shortUrlServiceV1.getLongUrl(shortUrl);
         return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY)
-            .header("Location", longUrl)
+            .location(URI.create(longUrl))
             .build();
     }
 }
