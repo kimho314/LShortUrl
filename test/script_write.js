f1c7a08cc32625c4d86dcc1b2ac8cc0056528642
kimho314 <kimho314@gmail.com>
2025-03-28 16:25:48 +0900

    [feat] : modify db, tomcat connection setting, modify write stress test option

--- test/script_write.js
+++ test/script_write.js
@@ -2,14 +2,14 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 500,
+  vus: 1000,
   duration: '30s',
-  cloud: {
-    // Project: Default project
-    projectID: 3744276,
-    // Test runs with the same name groups test runs together.
-    name: 'Test (01/02/2025-20:42:50)'
-  }
+  // cloud: {
+  //   // Project: Default project
+  //   projectID: 3744276,
+  //   // Test runs with the same name groups test runs together.
+  //   name: 'Test (01/02/2025-20:42:50)'
+  // }
 };
 
 export default function() {
@@ -23,6 +23,7 @@ export default function() {
   //   longUrl: `http://localhost:8080/v1/luna-short-url/greeting?name=vu-90-iter-4`
   // });
   const headers = { 'Content-Type': 'application/json' };
-  http.post('http://host.docker.internal:8080/api/v1/data/shorten', payload, { headers });
+  let res = http.post('http://host.docker.internal:8080/api/v1/data/shorten', payload, { headers });
+  // console.log(res.error);
   sleep(1);
 }
